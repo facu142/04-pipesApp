4eaf875274ec128407d692dba5bb9a5e404d4d72
Facundo Montenegro <facumonte29@gmail.com>
2021-04-16 00:27:26 -0300

    agrega date pipe , upper , lower y title case y cambia el idioma a español

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
+// Módulo personalizado
 
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el local de la app
+
+import localEs from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localEs)
 
 @NgModule({
   declarations: [
@@ -13,9 +22,14 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-AR' 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
